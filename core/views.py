bbc37a1941d7579a9468ba5630431eb7b02c316c
PRAJIN <prajinrp6@gmail.com>
2026-08-01 15:01:55 +0530

    Updated homepage and views

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render 
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home.html")
+    return HttpResponse("Hello from core App!")
