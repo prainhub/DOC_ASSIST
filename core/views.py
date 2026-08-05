ef52595c906e270e1c33d80e375ebf11a948749b
PRAJIN <prajinrp6@gmail.com>
2026-08-05 19:41:33 +0530

    Complete landing page with hero and features

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello from core App!")
+    return render(request,'home.html')
